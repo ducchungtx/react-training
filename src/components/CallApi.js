7ec3a3ba10506c9eec68f60f3b2df80fc4070baf
Chung Nguyen <zrmedia9@gmail.com>
2019-03-19 16:52:47 +0700

    Update code

--- src/components/CallApi.js
+++ src/components/CallApi.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
-import { Card, Form, Input, Row, Col} from 'antd';
+import { Card, Form, Input, Row, Col, Button} from 'antd';
 
 export class CallApi extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            userName: 'ducchungtx'
+        }
+        this.changeUrl = this.changeUrl.bind(this);
+        this.callApi = this.callApi.bind(this);
+        this.btnCallAction = this.btnCallAction.bind(this);
+    }
+
+    componentDidMount() {
+        this.callApi('ducchungtx');
+    }
+
+    changeUrl(e) {
+        this.setState({ userName: e.target.value });
+    }
+
+    callApi(username) {
+        let gitHubLink = `https://api.github.com/users/${username}/repos`;
+        fetch(gitHubLink).then(response => response.json()).then(data => this.setState({ data }));
+    }
+
+    btnCallAction() {
+        this.callApi(this.state.userName);
+    }
+
   render() {
+      const { data } = this.state;
     return (
       <Row>
           <Col>
@@ -10,12 +40,21 @@ export class CallApi extends Component {
                 <Row>
                     <Col md={12}>
                         <Card title="API" size="small">
-
+                            <Form>
+                                <Input name="url" onBlur={ this.changeUrl } />
+                                <Button onClick={this.btnCallAction}>Gọi API</Button>
+                            </Form>
                         </Card>
                     </Col>
                     <Col md={12}>
                         <Card title="Kết quả" size="small">
-
+                            { data ?
+                            data.map((item, index) => {
+                                return(
+                                    <p key={index}>{item.url}</p>
+                                )
+                            })
+                            : null}
                         </Card>
                     </Col>
                 </Row>
